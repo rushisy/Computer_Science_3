fa882e9727b8e4cef49b1cb68aafb6e3c11da3ad
Rushi Yalamanchili <rushisy@gmail.com>
2020-10-19 15:04:17 -0500

    binary trees examples

---
+++ src/examples/IntTree.java
@@ -0,0 +1,7 @@
+package examples;
+
+// An IntTree object represents an entire binary tree of ints.
+public class IntTree {
+	private IntTreeNode overalleNode; // null for an empty tree
+
+}
---
+++ src/examples/IntTreeNode.java
@@ -0,0 +1,23 @@
+package examples;
+
+//An IntTreeNode object is one node in a binary tree of ints.
+public class IntTreeNode {
+
+	int data; // data stored at this node
+	IntTreeNode left; // reference to the left subtree
+	IntTreeNode right; // reference to the right subtree
+
+	public IntTreeNode(int data) {
+		this.data = data;
+	}
+
+	public IntTreeNode(int data, IntTreeNode left, IntTreeNode right) {
+		this.data = data;
+		this.left = left;
+		this.right = right;
+	}
+
+	public static void main(String[] args) {
+
+	}
+}
