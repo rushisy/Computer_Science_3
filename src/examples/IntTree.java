9156b65fb642916370c2bd0add016a1094899e6e
Rushi Yalamanchili <rushisy@gmail.com>
2020-10-19 15:43:51 -0500

    binary tree examples

--- src/examples/IntTree.java
+++ src/examples/IntTree.java
@@ -2,6 +2,32 @@ package examples;
 
 // An IntTree object represents an entire binary tree of ints.
 public class IntTree {
-	private IntTreeNode overalleNode; // null for an empty tree
+	private IntTreeNode overallRoot; // null for an empty tree
 
+	public void print() { // most methods are paired with private public pair
+		print(overallRoot); // mini runner method
+		System.out.println();
+	}
+
+	private void print(IntTreeNode root) {
+		if (root != null) {
+			print(root.left);
+			System.out.println(root.data);
+			print(root.right);
+		}
+	}
+
+	public void printContains() {
+		contains(overallRoot, 5);
+		System.out.println();
+	}
+
+	private boolean contains(IntTreeNode root, int key) {
+		if (root == null) {
+			return false;
+		} else if (root.data == key) {
+			return true;
+		}
+		return contains(root.left, key) || contains(root.right, key);
+	}
 }
