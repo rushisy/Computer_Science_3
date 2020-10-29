e34232db65b812d8dd7b6d019249652b2db2bf4a
Rushi Yalamanchili <rushisy@gmail.com>
2020-10-29 13:17:21 -0500

    map labs

---
+++ src/histogram/Histogram.java
@@ -0,0 +1,23 @@
+package histogram;
+
+import java.util.Map;
+
+public class Histogram {
+	private Map<String, Integer> histogram;
+
+	public Histogram() {
+		// call setSentence
+	}
+
+	public Histogram(String sent) {
+		// call setSentence
+	}
+
+	public void setSentence() {
+	}
+
+	public String toString() {
+		String output = "";
+		return output + "\n\n";
+	}
+}
---
+++ src/histogram/HistogramRunner.java
@@ -0,0 +1,9 @@
+package histogram;
+
+import java.io.IOException;
+
+public class HistogramRunner {
+	public static void main(String args[]) throws IOException {
+		// add test cases
+	}
+}
