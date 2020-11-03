53bafdeb355959d3920ee53ad5b7354ed3934293
Rushi Yalamanchili <rushisy@gmail.com>
2020-11-03 14:23:54 -0600

    hash labs

--- src/tableOfWords/Word.java
+++ src/tableOfWords/Word.java
@@ -3,24 +3,48 @@ package tableOfWords;
 public class Word {
 	private String theValue;
 
-	// write a constructor
-
+	/**
+	 * preferred constructor
+	 * 
+	 * @param theValue the word
+	 */
 	public Word(String theValue) {
 		this.theValue = theValue.trim();
 	}
 
+	/**
+	 * outputs theValue
+	 * 
+	 * @return String the word
+	 */
 	public String getValue() {
 		return theValue;
 	}
 
+	/**
+	 * dont know why we need this
+	 * 
+	 * @return boolean default is set to false
+	 */
 	public boolean equals() {
 		return false;
 	}
 
+	/**
+	 * outputs the hashcode
+	 * 
+	 * @return int the hashcode
+	 */
 	public int hashCode() {
 		return (getVowels(theValue) * theValue.length()) % 10;
 	}
 
+	/**
+	 * helper method that returns the number of vowels in the input
+	 * 
+	 * @param word the input
+	 * @return int the number of vowels present in the word
+	 */
 	private int getVowels(String word) {
 		int counter = 0;
 		for (int i = 0; i < word.length(); i++) {
@@ -32,6 +56,11 @@ public class Word {
 		return counter;
 	}
 
+	/**
+	 * used for debugging
+	 * 
+	 * @return String the hashcode and the word
+	 */
 	public String toString() {
 		return theValue + " " + hashCode();
 	}
