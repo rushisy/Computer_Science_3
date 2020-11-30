8fc8ce4b405b03c2400ac1a58982f6cbc085be68
Rushi Yalamanchili <rushisy@gmail.com>
2020-11-29 20:32:25 -0600

    backtracking labs

--- src/majorLab/BoggleSolver.java
+++ src/majorLab/BoggleSolver.java
@@ -1,35 +1,83 @@
 package majorLab;
-public class BoggleSolver
-{
-	// Initializes the data structure using the given array of strings as the dictionary.
-	// (You can assume each word in the dictionary contains only the uppercase letters A - Z.)
-	public BoggleSolver(String dictionaryName)
-	{
-		//TODO
+
+import java.io.File;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class BoggleSolver {
+	private Set<String> dictionary;
+	private Set<String> validWords;
+	private boolean[][] visited;
+	private BoggleBoard board;
+
+	public BoggleSolver(String dictionaryName) {
+		try {
+			dictionary = new HashSet<String>();
+			validWords = new HashSet<String>();
+			Scanner key = new Scanner(new File(dictionaryName));
+			while (key.hasNextLine())
+				dictionary.add(key.nextLine());
+		} catch (Exception e) {
+			System.out.println("File not found!");
+		}
 	}
 
-	// Returns the set of all valid words in the given Boggle board, as an Iterable object
-	public Iterable<String> getAllValidWords(BoggleBoard board)
-	{
-		//TODO
-
-		return null;
+	public Iterable<String> getAllValidWords(BoggleBoard board) {
+		this.board = board;
+		visited = new boolean[board.rows()][board.cols()];
+		for (int i = 0; i < board.rows(); i++)
+			for (int j = 0; j < board.cols(); j++)
+				helper(i, j, 0, "");
+		return validWords;
 	}
 
-	// Returns the score of the given word if it is in the dictionary, zero otherwise.
-	// (You can assume the word contains only the uppercase letters A - Z.)
-	public int scoreOf(String word)
-	{
-		//TODO
+	public void helper(int i, int j, int size, String word) {
+		if (size >= 3 && dictionary.contains(word))
+			validWords.add(word);
 
+		if (i < 0 || i >= board.rows() || j < 0 || j >= board.cols() || visited[i][j])
+			return;
+		visited[i][j] = true;
+		int add = 1;
+		if (addition(i, j).equals("Qu"))
+			add = 2;
+		helper(i + 1, j, size + add, word + addition(i, j));
+		helper(i - 1, j, size + add, word + addition(i, j));
+		helper(i, j + 1, size + add, word + addition(i, j));
+		helper(i, j - 1, size + add, word + addition(i, j));
+		helper(i + 1, j + 1, size + add, word + addition(i, j));
+		helper(i + 1, j - 1, size + add, word + addition(i, j));
+		helper(i - 1, j - 1, size + add, word + addition(i, j));
+		helper(i - 1, j + 1, size + add, word + addition(i, j));
+		visited[i][j] = false;
+	}
+
+	public String addition(int i, int j) {
+		if (board.getLetter(i, j) == 'Q')
+			return "Qu";
+		return board.getLetter(i, j) + "";
+	}
+
+	public int scoreOf(String word) {
+		if (word.length() <= 4)
+			return 1;
+		else if (word.length() == 5)
+			return 2;
+		else if (word.length() == 6)
+			return 3;
+		else if (word.length() == 7)
+			return 5;
+		else if (word.length() >= 8)
+			return 11;
 		return 0;
 	}
 
 	public static void main(String[] args) {
 		System.out.println("WORKING");
 
-		final String PATH   = "./data/";
-		BoggleBoard  board  = new BoggleBoard(PATH + "board-q.txt");
+		final String PATH = "";
+		BoggleBoard board = new BoggleBoard(PATH + "board-q.txt");
 		BoggleSolver solver = new BoggleSolver(PATH + "dictionary-algs4.txt");
 
 		int totalPoints = 0;
@@ -39,9 +87,6 @@ public class BoggleSolver
 			totalPoints += solver.scoreOf(s);
 		}
 
-		System.out.println("Score = " + totalPoints); //should print 84
-
-		//new BoggleGame(4, 4);
+		System.out.println("Score = " + totalPoints);
 	}
-
 }
